06a959161a277e126578bb80541f1d494fcde182
thewanionly <wani.pitogo@gmail.com>
2022-08-03 07:07:22 +0800

    Use FilterDropdown in HomePage. Style the top area

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,32 @@
 import { useState } from 'react'
 
 import SearchBar from '../../components/SearchBar'
+import FilterDropdown from '../../components/FilterDropdown'
 
 import './HomePage.style.scss'
+
+const REGIONS = [
+  {
+    label: 'Africa',
+    value: 'africa'
+  },
+  {
+    label: 'America',
+    value: 'america'
+  },
+  {
+    label: 'Asia',
+    value: 'asia'
+  },
+  {
+    label: 'Europe',
+    value: 'europe'
+  },
+  {
+    label: 'Oceania',
+    value: 'oceania'
+  }
+]
 
 const HomePage = () => {
   const [searchTerm, setSearchTerm] = useState('')
@@ -14,13 +38,20 @@ const HomePage = () => {
   return (
     <div className='home-page' data-testid='home-page'>
       <div className='container home-page__container'>
-        <SearchBar
-          className='home-page__search-bar'
-          data-testid='country-search-bar'
-          placeholder='Search for a country...'
-          value={searchTerm}
-          onChange={handleSearchTerm}
-        />
+        <div className='home-page__top-area'>
+          <SearchBar
+            className='home-page__search-bar'
+            data-testid='country-search-bar'
+            placeholder='Search for a country...'
+            value={searchTerm}
+            onChange={handleSearchTerm}
+          />
+          <FilterDropdown
+            className='home-page__filter-dropdown'
+            label='Filter by Region'
+            menuItems={REGIONS}
+          />
+        </div>
         <div className='home-page__countries-list' data-testid='countries-list'></div>
       </div>
     </div>
