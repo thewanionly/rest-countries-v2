34aeb65e7d12b79aa4c6e9578b10455fc8f7d611
thewanionly <wani.pitogo@gmail.com>
2022-08-05 04:31:35 +0800

    Support for left and right icons in TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,4 +1,6 @@
 import { forwardRef } from 'react'
+
+import Icon, { IconProps } from '../Icon'
 import './TextInput.style.scss'
 
 type TextInputProps = {
@@ -9,20 +11,36 @@ type TextInputProps = {
   onChange?: (event: React.FormEvent<HTMLInputElement>) => void
   required?: boolean
   disabled?: boolean
+  iconLeft?: IconProps
+  iconRight?: IconProps
 }
 
 const TextInput = forwardRef<HTMLDivElement, TextInputProps>(
-  ({ className = '', ...props }, ref) => {
+  ({ className = '', iconLeft, iconRight, ...props }, ref) => {
     const textInputClassName = `text-input ${className}`
 
     return (
       <div ref={ref} className={textInputClassName}>
+        {!!iconLeft && (
+          <Icon
+            className={`text-input__icon ${iconLeft.className || ''}`}
+            name={iconLeft.name}
+            onClick={iconLeft.onClick}
+          />
+        )}
         <input
           className='text-input__field'
           data-testid='text-input-field'
           type='text'
           {...props}
         />
+        {!!iconRight && (
+          <Icon
+            className={`text-input__icon ${iconRight.className || ''}`}
+            name={iconRight.name}
+            onClick={iconRight.onClick}
+          />
+        )}
       </div>
     )
   }
