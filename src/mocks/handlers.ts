f8e81b5368e5dc44ba64c57bd6bca11689c9948c
thewanionly <wani.pitogo@gmail.com>
2022-08-09 07:26:09 +0800

    Update error in determining which handler is regions and which are for countries

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -4,31 +4,57 @@ import { rest } from 'msw'
 import { COUNTRIES_ALL } from '../utilities/constants'
 import { mockedCountries, mockedRegions } from './data'
 
-export const fetchAllCountries = () => {
+export const fetchAll = () => {
   return rest.get(`${COUNTRIES_ALL}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockedCountries))
-  })
-}
+    if (req.url.searchParams.get('fields') === 'region') {
+      return res(ctx.status(200), ctx.json(mockedRegions))
+    }
 
-export const fetchAllRegions = () => {
-  return rest.get(`${COUNTRIES_ALL}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockedRegions))
+    return res(ctx.status(200), ctx.json(mockedCountries))
   })
 }
 
 export const fetchAllCountriesError = () => {
   return rest.get(`${COUNTRIES_ALL}`, (req, res, ctx) => {
+    if (req.url.searchParams.get('fields') === 'region') {
+      return res(ctx.status(200), ctx.json(mockedRegions))
+    }
+
     return res(ctx.status(400), ctx.json({}))
   })
 }
 
 export const fetchAllCountriesEmpty = () => {
   return rest.get(`${COUNTRIES_ALL}`, (req, res, ctx) => {
+    if (req.url.searchParams.get('fields') === 'region') {
+      return res(ctx.status(200), ctx.json(mockedRegions))
+    }
+
     return res(ctx.status(200), ctx.json([]))
   })
 }
 
-export const handlers = [fetchAllCountries(), fetchAllRegions()]
+export const fetchAllRegionsError = () => {
+  return rest.get(`${COUNTRIES_ALL}`, (req, res, ctx) => {
+    if (req.url.searchParams.get('fields') === 'region') {
+      return res(ctx.status(400), ctx.json({}))
+    }
+
+    return res(ctx.status(200), ctx.json(mockedCountries))
+  })
+}
+
+export const fetchAllRegionsEmpty = () => {
+  return rest.get(`${COUNTRIES_ALL}`, (req, res, ctx) => {
+    if (req.url.searchParams.get('fields') === 'region') {
+      return res(ctx.status(200), ctx.json([]))
+    }
+
+    return res(ctx.status(200), ctx.json(mockedCountries))
+  })
+}
+
+export const handlers = [fetchAll()]
 
 //  // Handles a GET `{COUNTRIES_DETAIL}{cca2}`
 //  rest.get(`${COUNTRIES_DETAIL}:cca2`, null),
